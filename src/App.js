bc2c298aafa5270de6234009fe701fd1774b72e0
Ahmed Elsayed <ahmedelsayed6179@gmail.com>
2025-11-12 19:18:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import AppNavbar from "./components/appnavbar";
+import { Routes, Route } from "react-router";
+import Products from "./components/products";
+import Cart from "./components/cart";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppNavbar />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
